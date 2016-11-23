956bdd146c4beef6e31e8ce7f88ed8287368afd9
hayes9321 <t.hayes8321@gmail.com>
2016-11-22 17:17:20 -0800

    modified style and view added options to carousel

--- public/js/controller.js
+++ public/js/controller.js
@@ -40,8 +40,9 @@ angular.module('appCtrl', [])
     slides.push({
       image: ['public/css/images/fbpic.png', 'public/css/images/pack-that.png','public/css/images/pokemon-phaser.jpg', 'public/css/images/portfolio.png'][slides.length % 4],
       text: ['FACEBOOK COMMENT GENERATOR','PACK THAT','POKEMON PHASER','PORTFOLIO'][slides.length % 4],
-      code:["https://www.google.com","https://repl.it/languages/javascript","https://www.google.com","https://www.google.com"][slides.length % 4],
-      code:["https://www.google.com","https://repl.it/languages/javascript","https://www.google.com","https://www.google.com"][slides.length % 4],
+      content: ['FACEBOOK COMMENT GENERATOR','PACK THAT','POKEMON PHASER','PORTFOLIO'][slides.length % 4],
+      code:["https://github.com/hayes9321/facebook2","https://github.com/hayes9321/mean_hackathon","https://github.com/hayes9321/project-1","https://github.com/hayes9321/portfolioV1"][slides.length % 4],
+      demo:["#","#","#","#"][slides.length % 4],
       id: currIndex++
     });
   };
