87de13f36698805487b75e69e604d0d4897de5a8
hayes9321 <t.hayes8321@gmail.com>
2016-12-04 12:52:40 -0800

    converted work to mobile

--- public/js/controller.js
+++ public/js/controller.js
@@ -15,18 +15,6 @@ angular.module('appCtrl', [])
     var contact = angular.element(document.getElementById('Contact'));
       $document.scrollToElement(contact, 0, duration);
     };
-
-//handle images hiding and showing on demo screen
-   angular.element(document).ready(function(){
-    $scope.imgClass = []
-    $scope.imgClass.push('hide'); //on document load, hide image until mouseover event 
-      $scope.showBigImg = function (imgsrc){ //handle class change for css animations
-          $scope.imgClass.pop('hide') && $scope.imgClass.push('show');
-        }
-      $scope.hideBigImg = function(imgsrc){ //re-hide image until mouseover event happens again
-          $scope.imgClass.pop('show') && $scope.imgClass.push('hide');
-        }
-    });
 }])
 .controller('CarouselDemoCtrl', function ($scope) {
   $scope.myInterval = 5000;
